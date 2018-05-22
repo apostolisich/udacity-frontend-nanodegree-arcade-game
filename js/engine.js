58dbd1f96f460779cc5b77f872b176c4b9e4f0e8
tolisich <it14126@uom.edu.gr>
2018-05-22 13:51:02 +0300

    Fixed a typo
    One of the paths at the resources inside the engine.js file had at typo, leading to FILE_NOT_FOUND exception.

--- js/engine.js
+++ js/engine.js
@@ -173,8 +173,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png',
-        'images/Gem-Blue.png'
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
